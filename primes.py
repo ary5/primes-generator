7ecda5cdad931a8b643f75f7639b19e0be626981
Aryaman Saini <83552832+ary5@users.noreply.github.com>
2021-05-17 00:48:38 +0700

    Create primes.py

---
+++ primes.py
@@ -0,0 +1,21 @@
+import time
+primes = [2]
+n = int(input("Enter number"))
+tic = time.perf_counter()
+counter=0
+for x in range(3, n+1):
+    for y in range(0, len(primes)-1):
+        if(x%primes[y]==0):
+            counter=counter+1
+    if counter==0:
+        primes.append(x)
+        #print(x, end = " ")
+        counter=0
+    else:
+        counter=0
+toc = time.perf_counter()
+print(primes)
+print("Calculated in",(toc - tic), "seconds")
+    
+        
+
